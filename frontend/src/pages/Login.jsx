40ec3de80e608e8467f4e2972e58eb8f38579c39
Rodrigo_Vasquez <rvasquez26@gmail.com>
2024-09-20 22:46:19 -0300

    otra proeba de cors

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -36,6 +36,7 @@ const Login = () => {
           password
         },
         {
+          credentials: 'include',
           withCredentials: true // Ensure cookies are sent and handled
         }
       )
