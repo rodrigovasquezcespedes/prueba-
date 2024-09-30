14c9ab93902832add34da80d0a9213c0ff3cf0d0
Rodrigo_Vasquez <rvasquez26@gmail.com>
2024-09-30 20:00:26 -0300

    canvia parametros del backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
@@ -11,8 +10,6 @@ import favoriteRoutes from './routes/favoriteRoutes.js'
 import reportRoutes from './routes/reportRoutes.js'
 
 const app = express()
-
-dotenv.config()
 
 app.use(
   cors({
